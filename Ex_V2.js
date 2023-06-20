7e8aaae1c1cd2d0e8068ca827d111a1fc9de4082
Alejandro Luces González <aluces@polytex.local>
2023-06-19 23:57:38 -0400

    Exercise_codewars_V2

---
+++ Ex_V2.js
@@ -0,0 +1,14 @@
+// Implemente una función que convierta el valor booleano dado en su representación de cadena.
+
+
+
+
+const booleanToString=(b)=>{
+    //your code here
+    return b.toString()
+    
+  }
+
+  
+  console.log(booleanToString(true))
+  console.log(booleanToString(false))
