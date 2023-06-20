ce38ba161bd256d5afb1bc753f8f8b8212339251
Alejandro Luces González <aluces@polytex.local>
2023-06-20 14:04:04 -0400

    Exercise_codewars_V3

---
+++ Ex_V3.js
@@ -0,0 +1,39 @@
+// Contando ovejas...
+// Considere una matriz/lista de ovejas donde algunas ovejas pueden faltar en su lugar. 
+// Necesitamos una función que cuente el número de ovejas presentes en la matriz (verdadero significa presente).
+
+// Por ejemplo,
+let array1 =
+[true,  true,  true,  false,
+  true,  true,  true,  true ,
+  true,  false, true,  false,
+  true,  false, false, true ,
+  true,  true,  true,  true ,
+  false, false, true,  true]
+// La solución debe ser 17.
+
+// Hint: Don't forget to check for bad values like null/undefined
+
+    
+let count = 0
+const countSheeps =(arrayOfSheep) =>{
+
+    for (let i = 0; i < arrayOfSheep.length; i++) {
+        if (arrayOfSheep[i] === true) {
+            count++
+        }
+    }
+    return count
+}
+
+
+
+console.log(countSheeps(array1))
+
+
+
+
+
+
+
+// Path: Ex_V4.js
