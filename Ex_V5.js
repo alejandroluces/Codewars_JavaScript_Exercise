327d893cd38a801eea795f17c2910d56c6f7c30a
Alejandro Luces González <aluces@polytex.local>
2023-06-21 20:51:33 -0400

    Add exercise_codewars_V5_Modif

--- Ex_V5.js
+++ Ex_V5.js
@@ -23,7 +23,7 @@ console.log(PHRASE);
 // Otra forma de solucionar___________________
 
 const helloWorld2 = () => {
-    // feel free to change this into a function :)
+    // feel free to change this into a function 
         return String.fromCharCode(72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33);
     
   };
