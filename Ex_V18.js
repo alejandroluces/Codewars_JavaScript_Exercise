ff87c0f007fd4d9ddd8df8e4afa95730b2143e13
Alejandro Luces González <aluces@polytex.local>
2023-06-24 10:07:24 -0400

    Add_Excersise_codewars_V18

---
+++ Ex_V18.js
@@ -0,0 +1,11 @@
+// Complete la función que convierte un número binario (dado como una cadena) en un número decimal.
+
+const binToDec =(bin) =>{
+return parseInt(bin,2) 
+}
+
+console.log(binToDec([["1"]]));
+
+
+
+
