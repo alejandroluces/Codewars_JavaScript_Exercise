9ba21d1f6f196e496857d5b2b29ec4e03dda5037
Alejandro Luces González <aluces@polytex.local>
2023-06-24 00:28:43 -0400

    Add_exercise_codewars_V17

---
+++ Ex_V17.js
@@ -0,0 +1,11 @@
+// Escriba una función para dividir una cadena y convertirla en una matriz de palabras.
+
+
+
+
+const stringToArray =(string) =>{
+return  string.split(" ")
+}
+console.log(stringToArray("Robin Singh"));
+console.log(stringToArray("I love arrays they are my favorite"));
+
