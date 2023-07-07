19839dd1375677402b434cea21b73895a7f06018
Alejandro Luces González <aluces@polytex.local>
2023-07-07 13:18:36 -0400

    Add_Excersise_codewars_V47

---
+++ Ex_V47.js
@@ -0,0 +1,28 @@
+// Price of Mangoes
+// There's a "3 for 2" (or "2+1" if you like) offer on mangoes. For a given quantity and 
+// price (per mango), calculate the total cost of the mangoes.
+
+// Examples
+// mango(2, 3) ==> 6    # 2 mangoes for $3 per unit = $6; no mango for free
+// mango(3, 3) ==> 6    # 2 mangoes for $3 per unit = $6; +1 mango for free
+// mango(5, 3) ==> 12   # 4 mangoes for $3 per unit = $12; +1 mango for free
+// mango(9, 5) ==> 30   # 6 mangoes for $5 per unit = $30; +3 mangoes for fre
+
+const mango =(quantity, price) =>{
+
+    return (quantity - Math.floor(quantity / 3)) * price;
+
+}
+
+console.log(mango(88, 13));
+console.log(mango(5, 3));
+console.log(mango(63, 5));
+console.log(mango(83, 2))
+
+
+
+/* 
+1. Primero definimos una función llamada mango, con dos parámetros: cantidad y precio
+2. Devolvemos el producto de la diferencia entre la cantidad y el suelo del cociente 
+de la cantidad dividido por 3, y el precio 
+*/
