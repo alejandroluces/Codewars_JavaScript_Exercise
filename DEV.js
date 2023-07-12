798dc47e14bccdc53e2905ff47345f2455dd38e4
Alejandro Luces González <aluces@polytex.local>
2023-07-12 18:23:37 -0400

    dev

---
+++ DEV.js
@@ -0,0 +1,20 @@
+
+
+const oddCount =(n) =>{
+    
+    let result=[];
+        for (let i = 1; i < n; i++) {
+            
+                if (i%2!==0){
+                    result.push(i)
+            
+                }
+
+        }
+
+}
+
+
+console.log(oddCount(15));
+
+
