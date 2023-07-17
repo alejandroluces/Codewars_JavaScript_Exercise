419cbfeaf46f78ff090909194ec4d406cd9bc5a9
Alejandro M Luces G <alejandroluces5515@gmail.com>
2023-07-17 00:13:04 -0400

    Add exercise_Algoritmo_obj_06

---
+++ algoritmo_obj_06.js
@@ -0,0 +1,20 @@
+// 6)) Algoritmo para determinar si un objeto es vacío (no tiene propiedades):
+// Ejemplo:
+// Entrada: {}
+// Salida: true
+
+
+const objetoVacio  = 
+    {}
+    
+;
+
+
+const objVavio =(obj) =>{
+    if(obj={}){return true}
+    
+   
+
+}
+console.log(objVavio({}));
+
