39c4997c81f48b9af2ab4828cabb5acbafcc6734
Alejandro Luces González <aluces@polytex.local>
2023-08-01 14:34:05 -0400

    Add_Ex_codewars_V53

---
+++ Ex_V53.js
@@ -0,0 +1,40 @@
+// Los números triangulares se llaman así por la forma triangular equilátera que ocupan cuando se disponen como puntos. es decir
+
+// 1st (1)   2nd (3)    3rd (6)
+// *          **        ***
+//            *         **
+//                      *
+
+// Necesitas devolver el n-ésimo número triangular. Debe devolver 0 para valores fuera de rango:
+
+// Por ejemplo: ( Entrada --> Salida )
+
+// 0 --> 0
+// 2 --> 3
+// 3 --> 6
+// -10 --> 0
+// solucion:
+
+
+const triangular =(n) =>{
+
+    if (n <= 0) {
+        return 0;
+      } else {
+        return (n * (n + 1)) / 2;
+      }
+
+}
+
+/* 
+    //otra solucion:
+    function triangular( n ) {
+        return (n > 0) ? ((n * n) + n) / 2 : 0;
+      }
+*/
+
+console.log(triangular(2));
+console.log(triangular(4));
+console.log(triangular(-454));
+
+
