8fb412a34564118f0238b5ff7c5c98981e5be38e
Alejandro Luces González <aluces@polytex.local>
2023-08-02 11:58:11 -0400

    Add_Ex_codewars_V55

---
+++ Ex_V55.js
@@ -0,0 +1,18 @@
+// Instrucciones
+// Escriba una función que tome una sola cadena ( word) como argumento. La función debe devolver una lista ordenada que contenga los índices de todas las letras mayúsculas de la cadena.
+
+// Ejemplo (Entrada --> Salida)
+// "CodEWaRs" --> [0,3,4,6]
+
+// Solución
+const capitals =(word) =>{
+    let arr = [];
+    for(let i = 0; i < word.length; i++){
+      if(word[i] === word[i].toUpperCase()){
+        arr.push(i);
+      }
+    }
+    return arr;
+}
+console.log(capitals('CodEWaRs'));
+// console.log(capitals('AleJAndro'));
