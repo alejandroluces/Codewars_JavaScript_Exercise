20b67ce5314f7b4d5eb449244b51d87530b34139
Alejandro Luces González <aluces@polytex.local>
2023-08-03 23:17:50 -0400

    Add_Ex_codewars_V56

---
+++ Ex_V56.js
@@ -0,0 +1,15 @@
+// Pensar y probar: ¿A y B?
+// solucion codewars
+const testit =(a,b) =>{
+    return a|b
+    
+    }
+    
+    console.log(testit(0,1));
+    console.log(testit(1,2));
+    console.log(testit(10,20));
+    console.log(testit(1,1));
+    console.log(testit(0,0));
+    
+    // 2.	En la segunda línea, se retorna a|b, que es una operación 
+    // lógica que devuelve un 1 si al menos uno de los dos bits es 1, de lo contrario, devuelve un 0.
