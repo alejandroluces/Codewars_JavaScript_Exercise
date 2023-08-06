3d1da47a2a881a09a0d4f5828f8fda268a247512
Alejandro Luces González <aluces@LAPTOP-8FQ2FL12.polytex.local>
2023-08-05 21:09:51 -0400

    Add_Excersise_codewars_V62

--- Ex_V62.js
+++ Ex_V62.js
@@ -16,9 +16,13 @@ Only by Thinking and Testing
 Look at result of testcase, guess the code!
 */
 
-const testit=(n)=>{
-    return n.toString(2).replace(/0/g, '').length;
-}
+// const testit=(n)=>{
+//     return n.toString(2).replace(/0/g, '').length;
+// }
+
+function testit(n){
+    return n.toString(2).split('').filter(x => x == 1).length
+  }
 
 //Ookokokk?
 console.log(testit(0));
