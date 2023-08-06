8fc8b497d224770465fdded8133cd66a4a32c2de
Alejandro Luces González <aluces@LAPTOP-8FQ2FL12.polytex.local>
2023-08-05 20:43:18 -0400

    Add_Excersise_codewars_V62

---
+++ Ex_V62.js
@@ -0,0 +1,26 @@
+/* 
+Thinking & Testing : True or False
+711586% of 447504 of 1,943myjinxin20151 Issue Reported
+ JavaScript
+Node v8.1.3
+VIM
+EMACS
+Instructions
+Output
+No Story
+
+No Description
+
+Only by Thinking and Testing
+
+Look at result of testcase, guess the code!
+*/
+
+const testit=(n)=>{
+    return n.toString(2).replace(/0/g, '').length;
+}
+
+//Ookokokk?
+console.log(testit(0));
+console.log(testit(0));
+
