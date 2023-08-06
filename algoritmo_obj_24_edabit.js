b4df2d3b5e5dd25ffeb44214a5cfd45698781689
Alejandro Luces González <aluces@LAPTOP-8FQ2FL12.polytex.local>
2023-08-06 14:16:51 -0400

    Add_algoritmo_obj_24_edabit

---
+++ algoritmo_obj_24_edabit.js
@@ -0,0 +1,26 @@
+/* 
+ https://edabit.com/challenge/8q54MKnRrm89pSLmW
+
+ */
+
+let segundos = 60;
+ const minutCal =(n) =>{
+
+    return segundos * n;
+
+
+
+}
+console.log(minutCal(5));
+console.log(minutCal(3));
+
+
+
+
+
+
+
+
+
+
+
