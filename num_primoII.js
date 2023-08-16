59bb18a39f5ea4024890cad7508da981836e6e50
Alejandro Luces González <aluces@polytex.local>
2023-08-16 10:23:01 -0400

    Add_Ex_NumPrimo_II

---
+++ num_primoII.js
@@ -0,0 +1,12 @@
+const numeros = [2, 4, 7, 9, 11, 15, 16, 17, 18];
+//Extraer los numeros primos:
+const primos = numeros.filter((numero) => {
+    for (let i = 2; i < numero; i++) {
+        if (numero % i === 0) {
+        return false;
+        }
+    }
+    return numero !== 1;
+    }
+);
+console.log(primos); // [2, 7, 11, 17]
