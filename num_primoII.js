18faeef5cae11d94011de4a913e73b9ac263a9bf
Alejandro Luces González <aluces@polytex.local>
2023-08-30 16:06:14 -0400

    Add_Exaple

--- num_primoII.js
+++ num_primoII.js
@@ -10,3 +10,18 @@ const primos = numeros.filter((numero) => {
     }
 );
 console.log(primos); // [2, 7, 11, 17]
+
+
+// OTro Ejemplo:
+
+const numPrimo=(num)=>{
+    let primo=`Es Numero Primo ${num}`
+    for(let i=2;i<num;i++){
+        if(num%i===0){
+            primo=false
+        }
+    }
+    return primo
+}
+
+console.log(numPrimo(89));
